ca5d0d17936a7d2e6014c677e932659b949e7f6c
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2017-02-17 15:26:18 -0500

    add health system to level one

--- app/assets/javascripts/slimeLoader.js
+++ app/assets/javascripts/slimeLoader.js
@@ -1,13 +1,26 @@
+var rng = function() {
+  return Math.random();
+};
+
 var setupSlime = function(slime, scaler, animationSpeed) {
   slime.collideWorldBounds = true;
   slime.body.bounce.set(1);
   slime.health = 1;
   slime.enableBody = true;
+  slime.body.immovable = true;
   slime.animations.add('alive', [0, 1, 2, 1]);
   slime.animations.add('die', [0, 1, 0, 1, 2, 2]);
   slime.body.collideWorldBounds = true;
-  slime.body.velocity.x = Math.random()*80;
-  slime.body.velocity.y = Math.random()*-80;
   slime.animations.play('alive', animationSpeed, true);
   slime.scale.setTo(scaler, scaler);
+  if (rng() >= 0.5){
+    slime.body.velocity.x = (rng()+2)*50;
+  } else {
+    slime.body.velocity.x = (rng()-2)*50;
+  }
+  if (rng() >= 0.5){
+    slime.body.velocity.y = (rng()+2)*50;
+  } else {
+    slime.body.velocity.y = (rng()-2)*50;
+  }
 }
