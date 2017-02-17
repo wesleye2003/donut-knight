893a00db77ebefc6aa9f36c90893b39a7779a28f
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2017-02-17 12:36:36 -0500

    add basic level 3

---
+++ app/assets/javascripts/batLoader.js
@@ -0,0 +1,36 @@
+var rng = function() {
+  return Math.random();
+};
+
+var setupBat = function(bat, animationSpeed) {
+  bat.collideWorldBounds = true;
+  bat.body.bounce.set(1);
+  bat.health = 0.5;
+  bat.enableBody = true;
+  bat.animations.add('alive', [0, 1, 7, 6, 5]);
+  bat.animations.add('die', [7, 7, 5, 1, 0]);
+  bat.animations.add('walk', [3, 2, 6, 2, 3, 4]);
+  bat.body.collideWorldBounds = true;
+  bat.scale.setTo(1.5, 1.5);
+  bat.animations.play('alive', animationSpeed, false);
+  setTimeout(function(){
+    bat.health = 1;
+  }, 800);
+}
+
+function randomMovement(enemy) {
+  stopMovement(enemy);
+  if (rng() >= 0.5){
+    if (rng() >= 0.5){
+      enemy.body.velocity.x = (rng()+2)*50;
+    } else {
+      enemy.body.velocity.x = (rng()-2)*50;
+    }
+  } else {
+    if (rng() >= 0.5){
+      enemy.body.velocity.y = (rng()+2)*50;
+    } else {
+      enemy.body.velocity.y = (rng()-2)*50;
+    }
+  };
+}
