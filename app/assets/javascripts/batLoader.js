d7566b9cdeefc4941956cd705eb51f63434df395
wesleye2003 <el-amin.7@buckeyemail.osu.edu>
2017-02-20 15:30:02 -0500

    more refactors

--- app/assets/javascripts/batLoader.js
+++ app/assets/javascripts/batLoader.js
@@ -2,22 +2,9 @@ var rng = function() {
   return Math.random();
 };
 
-var setupBat = function(bat, animationSpeed) {
-  bat.collideWorldBounds = true;
-  bat.body.bounce.set(1);
-  bat.health = 0.5;
-  bat.enableBody = true;
-  bat.animations.add('alive', [0, 1, 7, 6, 5]);
-  bat.animations.add('die', [7, 7, 5, 1, 0]);
-  bat.animations.add('walk', [3, 2, 6, 2, 3, 4]);
-  bat.body.collideWorldBounds = true;
-  bat.body.immovable = true;
-  bat.scale.setTo(1.5, 1.5);
-  bat.animations.play('alive', animationSpeed, false);
-  setTimeout(function(){
-    bat.health = 1;
-  }, 1000);
-}
+var randomPosition =  function() {
+  return Math.random()*600;
+};
 
 function randomMovement(enemy) {
   stopMovement(enemy);
@@ -34,4 +21,26 @@ function randomMovement(enemy) {
       enemy.body.velocity.y = (rng()-2)*50;
     }
   };
+}
+
+var setupBat = function(game) {
+  var bat = game.add.sprite(randomPosition(), randomPosition(), 'bat');
+  bat.collideWorldBounds = true;
+  game.physics.enable(bat, Phaser.Physics.ARCADE);
+  bat.enableBody = true;
+  bat.body.bounce.set(1);
+  bat.health = 0.5;
+  bat.animations.add('alive', [0, 1, 7, 6, 5]);
+  bat.animations.add('die', [7, 7, 5, 1, 0]);
+  bat.animations.add('walk', [3, 2, 6, 2, 3, 4]);
+  bat.body.collideWorldBounds = true;
+  bat.body.immovable = true;
+  bat.scale.setTo(1.5, 1.5);
+  bat.animations.play('alive', 6, false);
+  setTimeout(function(){
+    bat.health = 1;
+  }, 1000);
+  game.time.events.repeat(Phaser.Timer.SECOND*5*rng(), 1000, randomMovement, game, bat);
+
+  return bat;
 }
--- app/assets/javascripts/bulletLoader.js
+++ app/assets/javascripts/bulletLoader.js
@@ -22,33 +22,3 @@ var setupBullets = function(game){
 function resetBullet (bullet) {
   bullet.kill();
 };
-
-// fires bullets(donuts) in different directions based on attacking direction
-// prevents additional shots based on 'bullet time' (can only fire once per second)
-// bullet time is there because idk how to make all bullets hit
-// the boss when more than one bullet is on the screen lol PLS FIX
-function fireBullet(direction) {
-  if (game.time.now > bulletTime){
-    bullet = bullets.getFirstExists(false);
-    game.add.tween(bullet).to( { angle: 360 }, 2000, Phaser.Easing.Linear.None, true);
-    if (bullet){
-      if (direction == "attackLeft"){
-        bullet.reset(knight.x - 5, knight.y);
-        bullet.body.velocity.x = -400;
-        bulletTime = game.time.now + 1000;
-      }else if (direction == "attackRight"){
-      	bullet.reset(knight.x + 5, knight.y);
-        bullet.body.velocity.x = 400;
-        bulletTime = game.time.now + 1000;
-      }else if (direction == "attackUp"){
-      	bullet.reset(knight.x, knight.y - 5);
-        bullet.body.velocity.y = -400;
-        bulletTime = game.time.now + 1000;
-      }else if (direction == "attackDown"){
-      	bullet.reset(knight.x, knight.y + 5);
-        bullet.body.velocity.y = 400;
-        bulletTime = game.time.now + 1000;
-      }
-    }
-  }
-}
